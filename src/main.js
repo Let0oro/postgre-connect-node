4cac602b58e09259fcc8341353f5661445a8452a
Let0oro <oniiichan354@gmail.com>
2023-11-09 02:25:50 +0100

    tutorial to connect from docker and pgAdmin 4 added

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
-/**
- Pasos a seguir para instalar Docker, PostgreSQL y establecer los ajustes iniciales
- */
-
-
-// Yo he instalado la biblioteca "dotenv" (npm i dotenv --save) -> https://www.npmjs.com/package/dotenv para esconder las variables en el repositorio público
+require('dotenv').config()
+const { Client } = require('pg');
+// He instalado la biblioteca "dotenv" (npm i dotenv --save) -> https://www.npmjs.com/package/dotenv para esconder las variables en el repositorio público
 // Además, he ignorado el archivo .env escribiendo su nombre en el archivo .gitignore
 // Por otro lado, para poder establecer la coneción, es necesaria la biblioteca "pg" (npm i pg) -> https://www.npmjs.com/package/pg 
-require('dotenv').config()
-const { Readline } = require('node:readline/promises');
-const { Client } = require('pg');
 
 // https://dev.to/mohsenkamrani/run-postgresql-with-docker-locally-and-connect-to-it-with-nodejs-451g
-/**
+// https://www.commandprompt.com/education/how-to-create-a-postgresql-database-in-docker/ 
+
+/** 
+ * CREAR TABLA CON DOCKER DESDE LA CONSOLA BASH
  * -> docker run --name [name-of-docker-container] -p 5432:5432 -e POSTGRES_PASSWORD=[password] -e POSTGRES_USER=[user] -d postgres
  * -> docker ps
  * -> docker exec -ti [name-of-docker-container] bash
@@ -22,25 +19,12 @@ const { Client } = require('pg');
  * -> CREATE TABLE tech_authors(ID INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, TYPE TEXT NOT NULL, CATEGORY TEXT NOT NULL, ATICLES INT NOT NULL);
  * -> INSERT INTO tech_authors VALUES (1, 'Laiba', 'Senior', 'Docker', 50);
  * -> SELECT * FROM tech_authors;
+
+
+ -> La tabla será alojada en el sistema y podrá verse en la app pgAdmin, usualmente en el servidor y con el usuario y contraseña principales.
+ En mi caso, se crea en un servidor llamado 'PostgreSQL 16', en la base de datos 'postgres';
+ Por esta razón, también podéis conectaros al servidor que creéis desde pgAdmin, crearos otra base de datos y nombrarla como queráis para conectaros a ella;
  */
-
-
-
-
-
-/**
- Pasos a seguir con docker:
- 1 - Abrir Docker Desktop
- 2 - Seleccionar "Imágenes" en la barra lateral izquierda
- 3 - Seleccionar el icono de "play" en el recuadro de "postgres"
- 4 - Abrir el menú expandible de las opciones adicionales
- 5 - Poner un nombre a nuestro nuevo contenedor
- 6 - Escribir un puerto (por defecto: 5432)
- 7 - Pulsar las opciones de "Host path", crear y seleccionar una carpeta (usualmente en Documentos>docker-volumes(así nombré yo a la que creé en ese momento))
- 8 - Escribir "POSTGRES_USER" en el input de la variable de entorno y en su valor, el rol con el que hayáis iniciado sesión al instalar PSQL
- 9 - Escribir "POSTGRES_PASSWORD" en el input de debajo, y, en su valor, la contraseña de vuestro rol  
- */
-
 
 // 10 - Definir el objeto que nos permitirá conectarnos:
 /**
@@ -58,39 +42,15 @@ const connectionData = {
     port: process.env.SECRET_PORT,
 }
 
-const schemaEmployees = `CREATE TABLE employees(
-    id INTEGER PRIMARY KEY,
-    name TEXT UNIQUE,
-    title TEXT NOT NULL
-);`;
-
-const schemaDepartaments = `CREATE TABLE departaments(
-    id INTEGER PRIMARY KEY,
-    name TEXT UNIQUE,
-    title TEXT NOT NULL
-);`;
-
-const createEmployees = () => client.query(schemaEmployees, showRows);
-const createDepartaments = () => client.query(schemaDepartaments, showRows);
-
-function alter (alteringThing, alteringName, typeAlter, alteredThing ,alteredName, alteredTyping) {
-    client.query(`${String(alteringThing).toUpperCase()}${String(alteringName).toLocaleLowerCase()}
-    ${String(typeAlter).toUpperCase()}${String(alteredThing).toUpperCase()}${String(alteredName).toLowerCase}${String(alteredTyping).toUpperCase()};`)
-} // alter no funciona en sqlite
-
-function pushTable (insertTableName, fields, values) {
-    const valueDecorator = (arr) => arr.map(v => typeof v == 'string' ? `'${v}'` : v).join(', ');
-    const query = `
-    INSERT INTO ${String(insertTableName).toLocaleLowerCase()}(${String(fields)})
-    VALUES (${valueDecorator(values)});`
-    client.query(query);
-}
-
 // 11 - Establecer conexión
 const client = new Client(connectionData);
+
+const simpleTypeQuery = (query) => {client.query(String(query), showRows)}
+
 client.connect().then(() => {
-    // pushTable('employees', 'id, name, age', [1, "Allan", 30])
-    client.query('SELECT * FROM employees', showRows);
+    simpleTypeQuery(`
+    ALTER TABLE employees
+    RENAME COLUMN title TO status;`)
 
 }).catch(err => {
     console.error(err);
@@ -103,10 +63,48 @@ const showRows = (err, res) => {
         console.log(err.message);
     } else {
         const fields = res.fields.map(v => v.name);
-        console.log(fields)
         console.log(res.rows)
     }
 
     client.end();
     process.exit(1);
 };
+
+
+// -----------------------------------------------------------------------------------------------
+const exampleSchemaEmployees = `CREATE TABLE employees(
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE,
+    title TEXT NOT NULL
+);`;
+
+const exampleSchemaDepartaments = `CREATE TABLE departaments(
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE,
+    title TEXT NOT NULL
+);`;
+
+const createEmployees = () => client.query(exampleSchemaEmployees, showRows);
+const createDepartaments = () => client.query(exampleSchemaDepartaments, showRows);
+
+const alterTable = (alteringThing, alteringName, typeAlter, alteredThing ,alteredName, alteredTyping) => {
+    client.query(`ALTER ${String(alteringThing).toUpperCase()}${String(alteringName).toLocaleLowerCase()}
+    ${String(typeAlter).toUpperCase()}${String(alteredThing).toUpperCase()}${String(alteredName).toLowerCase}${String(alteredTyping).toUpperCase()};`)
+} // alter no funciona en sqlite
+
+const pushTable = (insertTableName, fields, values) => {
+    const valueDecorator = (arr) => arr.map(v => typeof v == 'string' ? `'${v}'` : v).join(', ');
+    const query = `
+    INSERT INTO ${String(insertTableName).toLocaleLowerCase()}(${String(fields)})
+    VALUES (${valueDecorator(values)});`
+    client.query(query);
+}
+
+const updateTable = (insertTableName, changes, condition) => {
+    const valueDecorator = (arr) => arr.map(v => typeof v == 'string' ? `'${v}'` : v).join(', ');
+    const query = `
+    UPDATE ${String(insertTableName).toLocaleLowerCase()}
+    SET ${changes}
+    WHERE ${condition};`
+    client.query(query);
+}
